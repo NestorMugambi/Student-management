d70d5a764be94b723f04098f1ff320eb4da55ef5
NestorMugambi <arielnestor14@gmail.com>
2024-04-01 21:44:37 +0300

    practised sql search with world database

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,28 +23,59 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Button1 = New Button()
+        DataGridView1 = New DataGridView()
+        TextBox1 = New TextBox()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' Button1
         ' 
-        Button1.Location = New Point(436, 208)
+        Button1.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left
+        Button1.Location = New Point(180, 295)
         Button1.Name = "Button1"
-        Button1.Size = New Size(112, 34)
+        Button1.Size = New Size(112, 41)
         Button1.TabIndex = 0
         Button1.Text = "Button1"
         Button1.UseVisualStyleBackColor = True
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        DataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells
+        DataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells
+        DataGridView1.ColumnHeadersHeight = 34
+        DataGridView1.Location = New Point(12, 12)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 62
+        DataGridView1.Size = New Size(380, 223)
+        DataGridView1.TabIndex = 1
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left
+        TextBox1.Location = New Point(-6, 300)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(150, 31)
+        TextBox1.TabIndex = 2
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(10F, 25F)
         AutoScaleMode = AutoScaleMode.Font
+        AutoSize = True
         ClientSize = New Size(800, 450)
+        Controls.Add(TextBox1)
+        Controls.Add(DataGridView1)
         Controls.Add(Button1)
         Name = "Form1"
         Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
     Friend WithEvents Button1 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents TextBox1 As TextBox
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,15 +1,26 @@
 ﻿Imports MySql.Data.MySqlClient
+Imports Mysqlx
 Public Class Form1
     Dim conn As MySqlConnection
-    Dim COMMAND As MySqlCommand
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim COMMAND As MySqlCommand
+
+
         conn = New MySqlConnection
-        conn.ConnectionString = "server=localhost;userid=root;password=ness;database=students"
+        conn.ConnectionString = "server=localhost;userid=root;password=ness;database=world"
 
         Try
             conn.Open()
-            MessageBox.Show("Connection to MySQL test database was successful!!!!", "TESTING      CONNECTION TO MySQL DATABASE")
+            Dim strInput As String
+
+            Dim country As String = TextBox1.Text
+            Dim stm As String = $"SELECT `Name`, `Continent`, `Region`, `Population`, `LifeExpectancy` FROM country WHERE `Continent` = '{country}' "
+            Dim da As New MySqlDataAdapter(stm, conn)
+            Dim ds As New DataSet()
+            If da.Fill(ds) Then
+                DataGridView1.DataSource = ds.Tables(0)
+            End If
+
             conn.Close()
         Catch ex As Exception
             MsgBox(ex.Message)
@@ -18,3 +29,4 @@ Public Class Form1
 
     End Sub
 End Class
+
